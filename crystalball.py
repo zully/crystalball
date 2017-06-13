3243b7476969af426af5655e85380a8a10a01679
Chris Hidalgo <zully_1@yahoo.com>
2017-06-13 12:06:29 -0500

    initial commit

---
+++ crystalball.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+def crystal_ball():
+    answers = ['It is certain.',
+               'It is decidedly so.',
+               'Without a doubt', 
+               'Yes, definately!',
+               'You may rely on it.',
+               'As I see it, yes.',
+               'Most likely.',
+               'Outlook good.',
+               'Yes!',
+               'Signs point to yes.',
+               'Reply hazy, try again.',
+               'Ask again later.',
+               'Better not tell you now.',
+               'Cannot predict now.',
+               'Concentrate and ask again',
+               "Don't count on it.",
+               'My reply is no.',
+               'My sources say no',
+               'Outlook not so good.'
+               'Very doubtful.']
+
+    return random.choice(answers)
+
+def main():
+    print crystal_ball()
+    return
+
+if __name__ == '__main__':
+    main()
